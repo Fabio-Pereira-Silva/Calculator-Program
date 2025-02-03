d6521a4f9cd6bd40e5af1d5521da02cf364970e5
Fabio Pereira da Silva <fabio.silva@interplayers.com.br>
2025-02-03 14:04:44 -0300

    encapsulamento de metodo soma

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 
+static void Soma(){
+}
+
+{
 Console.Clear();
 Console.WriteLine("Primeiro Valor:");
 float v1 = float.Parse(Console.ReadLine());
-
 
 Console.WriteLine("Segundo Valor:");
 float v2 = float.Parse(Console.ReadLine());
@@ -12,4 +15,5 @@ Console.WriteLine("");
 
 float resultado = v1 + v2;
 Console.WriteLine("O resultado da soma é " + resultado);
+}
 
